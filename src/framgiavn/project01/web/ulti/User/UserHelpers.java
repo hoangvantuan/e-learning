b464a7454b8597220f287a86db153f1b5181f201
Vu Xuan Lam <lam.vuxuan2512@gmail.com>
2016-01-14 16:43:26 +0700

    add profile

--- src/framgiavn/project01/web/ulti/User/UserHelpers.java
+++ src/framgiavn/project01/web/ulti/User/UserHelpers.java
@@ -1,5 +1,8 @@
 package framgiavn.project01.web.ulti.User;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.opensymphony.xwork2.ActionContext;
 
 import framgiavn.project01.web.model.Password;
@@ -16,8 +19,17 @@ public final class UserHelpers {
 
     return (user.getPassword().equals(password.getOldPassword()));
   }
-  
+
   public static User getUserFromSession(String key) {
-    return (User)ActionContext.getContext().getSession().get(key);
+
+    return (User) ActionContext.getContext().getSession().get(key);
+  }
+
+  public static long getJoinedDay(Date date) {
+
+    Date currentTime = new Date();
+    long diff = currentTime.getTime() - date.getTime();
+    return diff / (24 * 60 * 60 * 1000);
+
   }
 }
